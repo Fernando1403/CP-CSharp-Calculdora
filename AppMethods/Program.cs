a81c8e1f19a1f0e27212eaf81db3bc4c7d217ecd
Fernando <84139372+Fernando1403@users.noreply.github.com>
2026-03-06 20:51:32 -0300

    Add files via upload

---
+++ AppMethods/Program.cs
@@ -0,0 +1,91 @@
+﻿int Somar(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int Subtrair(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int Multiplicar(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int Dividir(int num1, int num2)
+{
+    if (num1 == 0 || num2 == 0)
+    {
+        Console.WriteLine("Erro: divisão por zero não é permitida.");
+        return 0;
+    }
+
+    return num1 / num2;
+}
+
+Console.WriteLine("Calculadora (Adição / Subtração / Multiplicação / Divisão)");
+
+int escolha = 0;
+
+while (escolha != 5)
+{
+    Console.WriteLine("\nEscolha uma opção:");
+    Console.WriteLine("Escolha as opções: 1- Somar, 2- Subtrair, 3- Multiplicar, 4- Dividir, 5- Sair");
+
+    escolha = int.Parse(Console.ReadLine());
+
+    if (escolha == 1)
+    {
+        Console.WriteLine("Escolha o numero 1 da soma:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escolha o numero 2 da soma:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int resultado = Somar(num1, num2);
+
+        Console.WriteLine($"O resultado da soma é {resultado}");
+    }
+
+    if (escolha == 2)
+    {
+        Console.WriteLine("Escolha o numero 1 da subtração:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escolha o numero 2 da subtração:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int resultado = Subtrair(num1, num2);
+
+        Console.WriteLine($"O resultado da subtração é {resultado}");
+    }
+
+    if (escolha == 3)
+    {
+        Console.WriteLine("Escolha o numero 1 da multiplicação:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escolha o numero 2 da multiplicação:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int resultado = Multiplicar(num1, num2);
+
+        Console.WriteLine($"O resultado da multiplicação é {resultado}");
+    }
+
+    if (escolha == 4)
+    {
+        Console.WriteLine("Escolha o numero 1 da divisão:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escolha o numero 2 da divisão:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int resultado = Dividir(num1, num2);
+
+        Console.WriteLine($"O resultado da divisão é {resultado}");
+    }
+}
+
+Console.WriteLine("Programa encerrado.");
---
+++ AppMethods/Util.cs
@@ -0,0 +1,41 @@
+﻿namespace AppMethods
+{
+    public class Util
+    {
+        /// <summary>
+        /// 06.03.2026 - fernando.gonzales - method to sum numbers
+        /// </summary>
+        /// <param name="num1">parametro que recebe o primeiro valor</param>
+        /// <param name="num2">parametro que recebe o segundo valor</param>
+        /// <returns>Retorna a soma dos valores</returns>
+        public int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        /// <summary>
+        /// 06.03.2026 - fernando.gonzales - method to sum 3 numbers
+        /// </summary>
+        /// <param name="num1">parametro que recebe o primeiro valor</param>
+        /// <param name="num2">parametro que recebe o segundo valor</param>
+        /// <param name="num3">parametro que recebe o terceiro valor</param>
+        /// <returns></returns>
+        public int Somar(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        /// <summary>
+        /// 06.03.2026 - fernando.gonzales - method to sum 4 numbers
+        /// </summary>
+        /// <param name="num1">parametro que recebe o primeiro valor</param>
+        /// <param name="num2">parametro que recebe o segundo valor</param>
+        /// <param name="num3">parametro que recebe o terceiro valor</param>
+        /// <param name="num4">parametro que recebe o quarto valor</param>
+        /// <returns></returns>
+        public int Somar(int num1, int num2, int num3, int num4)
+        {
+            return num1 + num2 + num3 + num4;
+        }
+    }
+}
